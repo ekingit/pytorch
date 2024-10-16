4efeaed55ba9abf7fd0a5b24f516dc638be3e67f
Edward Z. Yang <ezyang@fb.com>
2024-10-16 16:18:41 -0400

    Update after_aot.py

--- torch/_dynamo/repro/after_aot.py
+++ torch/_dynamo/repro/after_aot.py
@@ -735,9 +735,9 @@ def repro_run(options, mod, load_args):
             if isinstance(arg, torch.Tensor) and arg.is_cuda:
                 need_sync = True
                 break
+        compiled(list(args))
         if need_sync:
             synchronize()  # ensure segfaults are surfaced
-    return lambda: compiled(list(args))
 
 
 # TODO: lazily load the inputs or something, rather than cloning them
