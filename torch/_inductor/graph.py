c432916a4a8e33f09895f1a6b19fbcc77504db24
chilli <chilli@meta.com>
2024-10-16 15:48:27 -0700

    Moved some of Inductor IR nodes to be frozen
    ghstack-source-id: cd242560e326ec384343538577ac936f81997e3e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137859

--- torch/_inductor/graph.py
+++ torch/_inductor/graph.py
@@ -1561,20 +1561,20 @@ class GraphLowering(torch.fx.Interpreter):
         # the origin_node here.
         if isinstance(result, TensorBox) and isinstance(result.data, ir.StorageBox):
             if isinstance(result.data.data, ir.Loops):
-                result.data.data.origin_node = n
+                result.data.data._post_init_setattr("origin_node", n)
             elif isinstance(result.data.data, ir.Buffer):
-                result.data.data.origin_node = n
+                result.data.data._post_init_setattr("origin_node", n)
                 if isinstance(result.data.data, ir.ComputedBuffer) and isinstance(
                     result.data.data.data, ir.Loops
                 ):
-                    result.data.data.data.origin_node = n
+                    result.data.data.data._post_init_setattr("origin_node", n)
                 # Not really multi-output, can straightforwardly recurse in
                 elif (
                     isinstance(result.data.data, ir.MultiOutput)
                     and not result.data.data.indices
                 ):
                     if isinstance(result.data.data.inputs[0], ir.Buffer):
-                        result.data.data.inputs[0].origin_node = n
+                        result.data.data.inputs[0]._post_init_setattr("origin_node", n)
 
         self.register_users_of(result)
 
@@ -1984,8 +1984,7 @@ class GraphLowering(torch.fx.Interpreter):
         return [
             node.get_name()
             for node in self.graph_outputs
-            if not isinstance(node, ir.NoneAsConstantBuffer)
-            and not isinstance(node, ir.ShapeAsConstantBuffer)
+            if not isinstance(node, ir.ExprAsConstantBuffer)
         ]
 
     def is_unspec_arg(self, name: str) -> bool:
